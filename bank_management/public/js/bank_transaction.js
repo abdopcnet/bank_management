80b0068e2e0a94939d94c4d629d75add0f6e1939
abdopcnet <abdopcnet@gmail.com>
2025-12-30 13:39:15 +0000

    🆕

---
+++ bank_management/public/js/bank_transaction.js
@@ -0,0 +1,34 @@
+// Copyright (c) 2025, abdopcnet@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bank Transaction', {
+	refresh(frm) {
+		// Add button to open Bank Reconcile Report in new window
+		if (!frm.is_new()) {
+			frm.add_custom_button(__('Bank Reconcile Report'), function () {
+				open_bank_reconcile_report_new_window(frm);
+			});
+		}
+	},
+});
+
+function open_bank_reconcile_report_new_window(frm) {
+	// Get default company
+	let company = frm.doc.company || frappe.defaults.get_default('company');
+
+	// Get bank account from current document
+	let bank_account = frm.doc.bank_account;
+
+	// Set route options for filters
+	frappe.route_options = {
+		company: company,
+	};
+
+	if (bank_account) {
+		frappe.route_options.bank_account = bank_account;
+	}
+
+	// Open in new window
+	frappe.open_in_new_tab = true;
+	frappe.set_route('query-report', 'Bank Reconcile Report');
+}
